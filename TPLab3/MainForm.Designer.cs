fa943d35869145583c571664d3d01d1efca065d2
ADMIN-PC <nilennkilim@gmail.com>
2025-05-11 14:54:24 +0300

    Реализовано чтение и отображение данных + графики + анализ трендов

--- TPLab3/MainForm.Designer.cs
+++ TPLab3/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace TPLab3
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Обязательная переменная конструктора.
@@ -28,13 +28,102 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(435, 394);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // chart1
+            // 
+            chartArea4.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.chart1.Legends.Add(legend4);
+            this.chart1.Location = new System.Drawing.Point(465, 12);
+            this.chart1.Name = "chart1";
+            series4.ChartArea = "ChartArea1";
+            series4.Legend = "Legend1";
+            series4.Name = "Series1";
+            this.chart1.Series.Add(series4);
+            this.chart1.Size = new System.Drawing.Size(474, 394);
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "chart1";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(465, 422);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(474, 24);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(12, 422);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(435, 260);
+            this.listBox1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(465, 466);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(474, 136);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Загрузить файл";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(948, 700);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "MainForm";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
     }
 }
 
--- TPLab3/MainForm.cs
+++ TPLab3/MainForm.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,11 +11,135 @@ using System.Windows.Forms;
 
 namespace TPLab3
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        private string currentFilePath;
+
+        public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "CSV files (*.csv)|*.csv";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                currentFilePath = openFileDialog.FileName;
+                string[] lines = File.ReadAllLines(currentFilePath);
+                LoadDataToGrid(lines);
+                PlotChart(lines);
+                AnalyzeTrends(lines);
+            }
+        }
+
+        private void LoadDataToGrid(string[] lines)
+        {
+            dataGridView1.Columns.Clear();
+            string[] headers = lines[0].Split(',');
+            foreach (string header in headers)
+            {
+                dataGridView1.Columns.Add(header, header);
+            }
+
+            for (int i = 1; i < lines.Length; i++)
+            {
+                dataGridView1.Rows.Add(lines[i].Split(','));
+            }
+        }
+
+        private void PlotChart(string[] lines)
+        {
+            chart1.Series.Clear();
+            string[] headers = lines[0].Split(',');
+
+            for (int col = 1; col < headers.Length; col++)
+            {
+                var series = chart1.Series.Add(headers[col]);
+                series.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+
+                for (int row = 1; row < lines.Length; row++)
+                {
+                    string[] values = lines[row].Split(',');
+                    int year = int.Parse(values[0]);
+                    double price = double.Parse(values[col]);
+                    series.Points.AddXY(year, price);
+                }
+            }
+        }
+
+        private void AnalyzeTrends(string[] lines)
+        {
+            listBox1.Items.Clear();
+            string[] headers = lines[0].Split(',');
+            string[] first = lines[1].Split(',');
+            string[] last = lines[lines.Length - 1].Split(',');
+
+            double maxDiff = double.MinValue;
+            double minDiff = double.MaxValue;
+            string mostIncreased = "";
+            string mostDecreased = "";
+
+            for (int i = 1; i < headers.Length; i++)
+            {
+                double diff = double.Parse(last[i]) - double.Parse(first[i]);
+
+                if (diff > maxDiff)
+                {
+                    maxDiff = diff;
+                    mostIncreased = headers[i];
+                }
+
+                if (diff < minDiff)
+                {
+                    minDiff = diff;
+                    mostDecreased = headers[i];
+                }
+            }
+
+            listBox1.Items.Add($"Сильнее всего подорожали: {mostIncreased} (+{maxDiff})");
+            listBox1.Items.Add($"Сильнее всего подешевели: {mostDecreased} ({minDiff})");
+        }
+
+        private void ForecastPrices(string[] lines, int yearsToForecast)
+        {
+            string[] headers = lines[0].Split(',');
+            int years = lines.Length - 1;
+            int windowSize = 3;
+
+            for (int col = 1; col < headers.Length; col++)
+            {
+                var forecastSeries = chart1.Series.Add(headers[col] + " (прогноз)");
+                forecastSeries.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+                forecastSeries.BorderDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dash;
+
+                List<double> prices = lines.Skip(1).Select(l => double.Parse(l.Split(',')[col])).ToList();
+
+                for (int i = 0; i < prices.Count; i++)
+                {
+                    forecastSeries.Points.AddXY(int.Parse(lines[i + 1].Split(',')[0]), prices[i]);
+                }
+
+                for (int i = 0; i < yearsToForecast; i++)
+                {
+                    double avg = prices.Skip(Math.Max(0, prices.Count - windowSize)).Take(windowSize).Average();
+                    int lastYear = int.Parse(lines[lines.Length - 1].Split(',')[0]);
+                    int nextYear = lastYear + i + 1;
+                    forecastSeries.Points.AddXY(nextYear, avg);
+                    prices.Add(avg);
+                }
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem != null && !string.IsNullOrEmpty(currentFilePath))
+            {
+                int forecastYears = int.Parse(comboBox1.SelectedItem.ToString());
+                string[] lines = File.ReadAllLines(currentFilePath);
+                ForecastPrices(lines, forecastYears);
+            }
         }
     }
 }
